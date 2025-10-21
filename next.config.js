dad93478f7c873a6004373a13ad5c9b0fbd8973d
Bernardo Belchior <bernardo@mui.com>
2025-10-21 16:58:29 +0100

    Avoid transpiling

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: [
-    "@mui/x-charts",
-    "@mui/x-charts-vendor",
-    "@mui/x-charts-pro",
-  ],
   eslint: {
     ignoreDuringBuilds: true,
   },
